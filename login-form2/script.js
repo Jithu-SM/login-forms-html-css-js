551554853890c334f5ef4812b5c69470dfda0ca5
Jithu S <82302841+Jithu-SM@users.noreply.github.com>
2025-03-14 11:25:15 +0530

    login form 2

---
+++ login-form2/script.js
@@ -0,0 +1,31 @@
+document.getElementById('toggle-theme').addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    const themeButton = document.getElementById('toggle-theme');
+    themeButton.textContent = document.body.classList.contains('dark') ? '🌜' : '🌞';
+});
+
+function createAnimationElements() {
+    const backgroundAnimation = document.querySelector('.background-animation');
+
+    for (let i = 0; i < 100; i++) {
+        const star = document.createElement('div');
+        star.className = 'stars';
+        star.style.setProperty('--i', i);
+        star.style.left = `${Math.random() * 100}%`;
+        backgroundAnimation.appendChild(star);
+
+        const snow = document.createElement('div');
+        snow.className = 'snow';
+        snow.style.setProperty('--i', i);
+        snow.style.left = `${Math.random() * 100}%`;
+        backgroundAnimation.appendChild(snow);
+
+        const dot = document.createElement('div');
+        dot.className = 'dots';
+        dot.style.setProperty('--i', i);
+        dot.style.left = `${Math.random() * 100}%`;
+        backgroundAnimation.appendChild(dot);
+    }
+}
+
+createAnimationElements();
